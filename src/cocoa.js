6191dd152a384517d2c583b7bcfd766175b2475d
ceegee171 <156711625+ceegee171@users.noreply.github.com>
2024-04-04 22:26:21 -0700

    API functionality updated

--- src/cocoa.js
+++ src/cocoa.js
@@ -1,3 +1,17 @@
+function refreshWeather(response) {
+  let temperature = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+}
+function searchCity(city) {
+  let apiKey = "1841a154a3444205bo994f2f03567t09";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key={apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-search-action");
@@ -7,3 +21,5 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#form-search");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Los Angeles");
