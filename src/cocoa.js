22f0af226861783da065c9ec8c679f90fb746d52
ceegee171 <156711625+ceegee171@users.noreply.github.com>
2024-04-06 20:06:27 -0700

    ionno

--- src/cocoa.js
+++ src/cocoa.js
@@ -4,11 +4,36 @@ function refreshWeather(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("description");
   let humidityElement = document.querySelector("humidity");
+  let windSpeedElement = document.querySelector("wind-speed");
+  late date = new Date (response.data.time * 1000); 
 
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = `${date.getDay()} ${date.getHours()} : ${date.getMinutes()}`; 
   descriptionElement = response.data.condition.description;
   humidityElement = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = response.data.wind.speed;
   temperatureElement.innerHTMl = Math.round(temperature);
+}
+
+function formatDate(date){
+    let day = date.getDay();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday", 
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]; 
+    let day = day[date.getDay()];
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+
+    
+    return `${day}, ${hours }:${minutes}`; 
 }
 function searchCity(city) {
   let apiKey = "1841a154a3444205bo994f2f03567t09";
@@ -19,11 +44,9 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-search-action");
-  let cityElement = document.querySelector("#women-weed-weather");
-  cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#form-search");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-searchCity("Los Angeles");
+searchFormElement.addEventListener("submit", handleSearchSubmit); 
