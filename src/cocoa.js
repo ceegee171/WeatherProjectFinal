52b42e50289e162811e4ad1913118f7fce5350dc
ceegee171 <156711625+ceegee171@users.noreply.github.com>
2024-04-13 15:48:58 -0700

    Finished?

--- src/cocoa.js
+++ src/cocoa.js
@@ -12,10 +12,9 @@ function refreshWeather(response) {
   timeElement.innerHTML = formatDate(date);
   descriptionElement = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+
   windSpeedElement.innerHTML = response.data.wind.speed;
   temperatureElement.innerHTML = Math.round(temperature);
-
-  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -54,6 +53,12 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp + 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "1841a154a3444205bo994f2f03567t09";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
@@ -62,33 +67,40 @@ function getForecast(city) {
 
 function displayForecast(response) {
   console.log(response.data);
-
   let days = ["Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
   let forecastHtml = "";
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
      <div class="weather-forecast-day">
           <div class="col-2">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon"> &#9728;&#65039;</div>
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <img src="${
+              day.condition.icon_url
+            }" class="weather-forecast-icon" /> 
             <div class="weather-forecast-temperature">
-              <span class="weather-forecast-temperature-max"><strong> 18°</strong></span>
-              <span class="weather-forecast-temperature-min"> <strong> 12°</strong></span>
+              <span class="weather-forecast-temperature-max"><strong> ${Math.round(
+                day.temperature.maximum
+              )}°</strong></span>
+              <span class="weather-forecast-temperature-min"> <strong> ${Math.round(
+                day.temperature.minimum
+              )}°</strong></span>
             </div>
           </div>
         </div>
         `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
 }
-
-let searchFormElement = document.querySelector("#form-search-action");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
 displayForecast();
 searchCity("Paris");
 getForecast();
+
+let searchFormElement = document.querySelector("#form-search-action");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
